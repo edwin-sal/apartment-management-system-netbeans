a8c21c27238538d91dfd7ed6c3ff46087973affe
edwin <edwinbanago692@gmail.com>
2023-12-01 22:34:55 +0800

    Created a frame to add expenses.

--- src/AddExpensePage.java
+++ src/AddExpensePage.java
@@ -18,6 +18,7 @@ public class AddExpensePage extends javax.swing.JFrame {
      */
     public AddExpensePage() {
 	initComponents();
+	setLocationRelativeTo(null);
     }
 
     /**
@@ -29,59 +30,106 @@ public class AddExpensePage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel2 = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        expenseInputPanel = new javax.swing.JPanel();
+        electricBillInput = new javax.swing.JSpinner();
+        electricBillLabel = new javax.swing.JLabel();
+        waterBillLabel = new javax.swing.JLabel();
+        waterBillInput = new javax.swing.JSpinner();
+        internetBillLabel = new javax.swing.JLabel();
+        internetBillInput = new javax.swing.JSpinner();
+        maintenanceBillLabel = new javax.swing.JLabel();
+        maintenanceBillInput = new javax.swing.JSpinner();
+        expenseInputLabel = new javax.swing.JLabel();
+        addExpenseGoBackButton = new javax.swing.JButton();
+        addExpenseButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        expenseInputPanel.setBackground(new java.awt.Color(255, 255, 255));
+        expenseInputPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        expenseInputPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        expenseInputPanel.add(electricBillInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 340, 40));
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        electricBillLabel.setFont(new java.awt.Font("Archivo SemiBold", 0, 15)); // NOI18N
+        electricBillLabel.setText("Electricity bill");
+        expenseInputPanel.add(electricBillLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 120, 30));
 
-        jLabel1.setText("Add Expense");
+        waterBillLabel.setFont(new java.awt.Font("Archivo SemiBold", 0, 15)); // NOI18N
+        waterBillLabel.setText("Water bill");
+        expenseInputPanel.add(waterBillLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 120, 120, 30));
+        expenseInputPanel.add(waterBillInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, 340, 40));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 180, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE)
-                    .addContainerGap()))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 50, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE))
-        );
+        internetBillLabel.setFont(new java.awt.Font("Archivo SemiBold", 0, 15)); // NOI18N
+        internetBillLabel.setText("Internet bill");
+        expenseInputPanel.add(internetBillLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 220, 120, 30));
+        expenseInputPanel.add(internetBillInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, 340, 40));
 
-        jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, -20, 180, 50));
+        maintenanceBillLabel.setFont(new java.awt.Font("Archivo SemiBold", 0, 15)); // NOI18N
+        maintenanceBillLabel.setText("Maintenance bill");
+        expenseInputPanel.add(maintenanceBillLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, 120, 30));
+        expenseInputPanel.add(maintenanceBillInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 350, 340, 40));
+
+        expenseInputLabel.setFont(new java.awt.Font("Archivo SemiBold", 0, 28)); // NOI18N
+        expenseInputLabel.setText("Add Expense");
+
+        addExpenseGoBackButton.setBackground(new java.awt.Color(254, 254, 254));
+        addExpenseGoBackButton.setText("Back");
+        addExpenseGoBackButton.setFocusable(false);
+        addExpenseGoBackButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addExpenseGoBackButtonActionPerformed(evt);
+            }
+        });
+
+        addExpenseButton.setBackground(new java.awt.Color(254, 254, 254));
+        addExpenseButton.setText("Add");
+        addExpenseButton.setFocusable(false);
+        addExpenseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addExpenseButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(15, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(expenseInputLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addComponent(addExpenseGoBackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(addExpenseButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(expenseInputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 381, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(38, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 593, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15))
+                .addContainerGap(20, Short.MAX_VALUE)
+                .addComponent(expenseInputLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(expenseInputPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(addExpenseGoBackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(addExpenseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(75, 75, 75))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void addExpenseGoBackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addExpenseGoBackButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_addExpenseGoBackButtonActionPerformed
+
+    private void addExpenseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addExpenseButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_addExpenseButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -103,8 +151,17 @@ public class AddExpensePage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton addExpenseButton;
+    private javax.swing.JButton addExpenseGoBackButton;
+    private javax.swing.JSpinner electricBillInput;
+    private javax.swing.JLabel electricBillLabel;
+    private javax.swing.JLabel expenseInputLabel;
+    private javax.swing.JPanel expenseInputPanel;
+    private javax.swing.JSpinner internetBillInput;
+    private javax.swing.JLabel internetBillLabel;
+    private javax.swing.JSpinner maintenanceBillInput;
+    private javax.swing.JLabel maintenanceBillLabel;
+    private javax.swing.JSpinner waterBillInput;
+    private javax.swing.JLabel waterBillLabel;
     // End of variables declaration//GEN-END:variables
 }
